6c0dd0d7cabe4540435556183c9a6328b09ae3ce
ruvindiimesha <ruvindiimesha7855@gmail.com>
2024-10-25 09:44:25 +0530

    Add files via upload

---
+++ SequenceCollection.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.SequencedCollection;
+
+public class SeqCollec{
+	public SeqCollec(){
+		SequencedCollection sc = new LinkedList();
+		sc.addFirst(5);
+		sc.addFirst(2);
+		sc.addLast(3);
+		System.out.println(sc.getFirst());
+		System.out.println(sc.removeLast());
+		System.out.println(sc);
+		System.out.println(sc.reversed());
+	}
+	public static void main(String[] args){
+		new SeqCollec();
+		
+	}
+}
